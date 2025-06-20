8129f585328927cc19455db466ba6c0607a53403
Jojo823-svg <joy.mutinda@strathmore.edu>
2025-06-20 11:58:37 +0300

    did bubble and selection sort

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,23 @@
-def bubble_sort(unsorted_list):
-    # TODO: Implement bubble sort
-    pass
+def bubble_sort(arr,n):
+    n=len(arr)
+    for i in range(n):
+        swapped = False
+        for j in range(0, n-i-1):
+            if arr[j]>arr[j+1]:
+                arr[j],arr[j+1]=arr[j+1],arr[j]
+                swapped=True
+        if not swapped:
+            break
+
+my_num=[5,13,6,2,12,9,15,8]
+print("LIst: ",my_num)
+
+bubble_sort(my_num,len(my_num))
+print("Sorted list: ",my_num)
+
+
+
+
+
+
+
--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,17 @@
 def selection_sort(arr):
-    # TODO: Implement selection sort
-    pass
+   n=len(arr)
+   for i in range(n):
+       min_index=i
+       for j in range(i+1,n):
+           if arr[j]<arr[min_index]:
+               min_index=j
+               arr[i],arr[min_index]=arr[min_index],arr[i]
+
+
+my_num=[2,4,1,5,3]
+print("List: ",my_num)
+
+selection_sort(my_num)
+print("Sorted list: ",my_num)
+
+
